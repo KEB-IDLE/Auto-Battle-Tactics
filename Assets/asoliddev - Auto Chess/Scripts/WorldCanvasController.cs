e4675439ddb3a6d229f0422a68e02a7c8c2119fe
tmdgur23 <dltmdgur08@naver.com>
2025-08-20 23:36:50 +0900

    tts 유닛 전체 적용

--- Assets/asoliddev - Auto Chess/Scripts/WorldCanvasController.cs
+++ Assets/asoliddev - Auto Chess/Scripts/WorldCanvasController.cs
@@ -10,30 +10,40 @@ public class WorldCanvasController : MonoBehaviour
     public GameObject worldCanvas;
     public GameObject floatingTextPrefab;
     public GameObject healthBarPrefab;
-   
-    /// <summary>
-    /// For Creating a new FloatingText
-    /// </summary>
-    /// <param name="position"></param>
-    /// <param name="v"></param>
+
     public void AddDamageText(Vector3 position, float v)
     {
-        GameObject go = Instantiate(floatingTextPrefab);
-        go.transform.SetParent(worldCanvas.transform);
-       
-        go.GetComponent<FloatingText>().Init(position, v);
+        if (!worldCanvas || !floatingTextPrefab)
+        {
+            Debug.LogError("[WorldCanvasController] worldCanvas/floatingTextPrefab not set");
+            return;
+        }
+
+        var go = Instantiate(floatingTextPrefab);
+        go.transform.SetParent(worldCanvas.transform, false); // ★
+        go.transform.localScale = Vector3.one;                // ★
+
+        var ft = go.GetComponent<FloatingText>();
+        if (ft) ft.Init(position, v);
+        else Debug.LogError("[WorldCanvasController] FloatingText component missing on prefab");
     }
 
-    /// <summary>
-    /// For Creating a new HealthBar
-    /// </summary>
-    /// <param name="position"></param>
-    /// <param name="v"></param>
-    public void AddHealthBar(GameObject championGO)
+    public GameObject AddHealthBar(GameObject championGO)
     {
-        GameObject go = Instantiate(healthBarPrefab);
-        go.transform.SetParent(worldCanvas.transform);
+        if (!worldCanvas || !healthBarPrefab)
+        {
+            Debug.LogError("[WorldCanvasController] worldCanvas/healthBarPrefab not set");
+            return null;
+        }
 
-        go.GetComponent<HealthBar>().Init(championGO);
+        var go = Instantiate(healthBarPrefab);
+        go.transform.SetParent(worldCanvas.transform, false); // ★
+        go.transform.localScale = Vector3.one;                // ★
+
+        var hb = go.GetComponent<HealthBar>();
+        if (hb) hb.Init(championGO);
+        else Debug.LogError("[WorldCanvasController] HealthBar component missing on prefab");
+
+        return go;
     }
 }
