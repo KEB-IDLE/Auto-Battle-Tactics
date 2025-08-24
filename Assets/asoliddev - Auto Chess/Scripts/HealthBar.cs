922e0b2dd53439f6952e11b4afa15bd7cc6f8089
tmdgur23 <dltmdgur08@naver.com>
2025-08-24 22:04:14 +0900

    update

--- Assets/asoliddev - Auto Chess/Scripts/HealthBar.cs
+++ Assets/asoliddev - Auto Chess/Scripts/HealthBar.cs
@@ -3,35 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-/// <summary>
-/// Displays champion health over champion
-/// </summary>
 public class HealthBar : MonoBehaviour
 {
     private GameObject championGO;
     private ChampionController championController;
     public Image fillImage;
 
+    [Header("Colors")]
+    [SerializeField] private Color playerColor = new Color(0.21f, 0.82f, 0.36f, 1f); // 초록
+    [SerializeField] private Color enemyColor  = new Color(0.93f, 0.20f, 0.20f, 1f); // 빨강
+
     private CanvasGroup canvasGroup;
 
-    /// Start is called before the first frame update
     void Start()
     {
         canvasGroup = this.GetComponent<CanvasGroup>();
     }
 
-    /// Update is called once per frame
     void Update()
     {
-        if(championGO != null)
+        if (championGO != null)
         {
-            this.transform.position = championGO.transform.position + new Vector3(0, 1.5f + 1.5f * championGO.transform.localScale.x, 0);
-            fillImage.fillAmount = championController.currentHealth / championController.maxHealth;
+            this.transform.position = championGO.transform.position
+                                      + new Vector3(0, 1.5f + 1.5f * championGO.transform.localScale.x, 0);
 
-            if (championController.currentHealth <= 0)
-                canvasGroup.alpha = 0;
-            else
-                canvasGroup.alpha = 1;
+            fillImage.fillAmount = championController.currentHealth / championController.maxHealth;
+            canvasGroup.alpha = (championController.currentHealth <= 0) ? 0 : 1;
         }
         else
         {
@@ -39,14 +36,16 @@ public class HealthBar : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Called when champion created
-    /// </summary>
-    /// <param name="_championGO"></param>
     public void Init(GameObject _championGO)
     {
         championGO = _championGO;
         championController = championGO.GetComponent<ChampionController>();
 
+        // ✅ 적(TEAMID_AI)이면 빨강, 아니면 초록
+        if (fillImage && championController)
+        {
+            bool isEnemy = (championController.teamID == ChampionController.TEAMID_AI);
+            fillImage.color = isEnemy ? enemyColor : playerColor;
+        }
     }
 }
